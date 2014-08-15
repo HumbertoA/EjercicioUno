300cc39ab969537dc027f95e1ee6177aa08622da
CCDM06 <LABORATORIOMAC@iMac-de-CCDM06.local>
2014-08-15 11:15:07 -0600

    calculadora

--- EjercicioUno/main.m
+++ EjercicioUno/main.m
@@ -13,8 +13,62 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *mensaje = @"Hola a la calculadora";
+        NSLog(@"%@", mensaje);
+        
+        float primerValor;
+        scanf("%f", &primerValor);
+        
+        int opcion;
+        scanf("%d", &opcion);
+        
+        float segundoValor;
+        scanf("%f", &segundoValor);
+        
+        float operacion = 0;
+        
+        switch (opcion) {
+            case 0:
+                operacion = (float)primerValor + (float)segundoValor;
+                break;
+            case 1:
+                operacion = (float)primerValor - (float)segundoValor;
+                break;
+            case 2:
+                operacion = (float)primerValor * (float)segundoValor;
+                break;
+            case 3:
+                operacion = (float)primerValor / (float)segundoValor;
+                break;
+            default:
+                NSLog(@"Opcion no valida!!");
+                break;
+        }
+        
+        if (operacion > 100) {
+            NSLog(@"mensaje 1");
+        }else{
+            NSLog(@"mensaje 2");
+        }
+        
+        NSMutableArray *arrayOperaciones = [[NSMutableArray alloc] init];
+        
+        NSMutableArray *array;
+        array = [NSMutableArray alloc];
+        array = [NSMutableArray init];
+        
+        NSNumber *numero;
+        numero = [NSNumber alloc];
+        numero = [[NSNumber alloc] initWithFloat:operacion];
+        
+        [array addObject:numero];
+        [array count];
+        
+        /*NSDictionary *dictionary = @{
+                                     @"nombre": @"Humberto",
+                                     @"edad": @"100",
+                                     @"lenguaje": @"Objetive-C"
+                                    };*/
         
     }
     return 0;
