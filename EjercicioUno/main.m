969f2d02bf445cb56870a8f90421a7400f6e418f
CCDM06 <LABORATORIOMAC@iMac-de-CCDM06.local>
2014-08-15 10:08:50 -0600

    Initial Commit

---
+++ EjercicioUno/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EjercicioUno
+//
+//  Created by CCDM06 on 15/08/14.
+//  Copyright (c) 2014 Humberto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
